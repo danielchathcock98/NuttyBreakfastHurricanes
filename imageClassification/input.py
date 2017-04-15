cb385c1fd64417373e74469f4b4729214e657b7e
Shyamal Patel <Shyamal Patel>
2017-04-15 01:21:38 -0400

    Windows Compatability

--- imageClassification/input.py
+++ imageClassification/input.py
@@ -18,13 +18,15 @@ def readImages():
 
 
     for part in os.listdir(TRAIN_DIR):
-        partPath = os.path.join(TRAIN_DIR, part)
-        for filename in os.listdir(partPath):
-            filepath = os.path.join(partPath, filename)
+        if (part != '.DS_Store'):
+            partPath = os.path.join(TRAIN_DIR, part)
+            for filename in os.listdir(partPath):
+                if (partPath != '.DS_Store'):
+                    filepath = os.path.join(partPath, filename)
 
-            image = Image.open(filepath)
-            imageClass = labels[filename]
-            #print(imageClass)
+                    image = Image.open(filepath)
+                    imageClass = labels[filename]
+                    #print(imageClass)
 
 
 
