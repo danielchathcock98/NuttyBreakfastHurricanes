63f749900ce60a10f1830ca9d544e5323860c31f
Daniel Hathcock <danielchathcock98@gmail.com>
2017-04-15 01:06:13 -0400

    stuff 2

--- imageClassification/input.py
+++ imageClassification/input.py
@@ -2,5 +2,22 @@ from __future__ import with_statement
 
 from PIL import Image
 import numpy as np
+import os
+
+TRAIN_DIR = './HomeDepot/ImagesTrain'
+
+def readImages():
+    labels = {}
+
+    for part in os.listdir(TRAIN_DIR):
+        partPath = os.path.join(TRAIN_DIR, part)
+        for filename in os.listdir(partPath):
+            filepath = os.path.join(partPath, filename)
+
+            image = Image.open(filepath)
+            imageClass = labels[filename]
 
 
+
+
+readImages()
