56d81f4fa8ed0cc6db1b6e67267e2f2ed8f52e49
Shyamal Patel <Shyamal Patel>
2017-04-15 01:03:42 -0400

    Did stuff

--- imageClassification/input.py
+++ imageClassification/input.py
@@ -4,3 +4,10 @@ from PIL import Image
 import numpy as np
 
 
+labels = {}
+categoriesPath = './HomeDepot/categoriesTrain.txt';
+f = categories = open(categoriesPath, 'r')
+for line in f:
+    values = line.split('|')
+    labels[values[2].rstrip()] = values[1]
+print(labels.keys())
